7d3874da324da512bca2e868756f736c99fe62ab
dannysepler <dsepler@ufl.edu>
2014-08-12 02:49:32 -0400

    added bar graphs

---
+++ bootstrapped/data.js
@@ -0,0 +1,154 @@
+var operatingData = [
+    {
+      value: 29,
+      color:"#F7464A",
+      highlight: "#FF5A5E",
+      label: "Public Safety"
+    },
+    {
+      value: 22,
+      color: "#46BFBD",
+      highlight: "#5AD3D1",
+      label: "Neighborhood and Infrastructure"
+    },
+    {
+      value: 7,
+      color: "#FDB45C",
+      highlight: "#FFC870",
+      label: "General Government"
+    },
+    {
+      value: 5,
+      color: "#949FB1",
+      highlight: "#A8B3C5",
+      label: "Recreation and Culture"
+    },
+    {
+      value: 24,
+      color: "#4D5360",
+      highlight: "#616774",
+      label: "Transportation"
+    },
+    {
+      value: 9,
+      color: "#666",
+      highlight: "#616774",
+      label: "Health and Human Services"
+    },
+    {
+      value: 3,
+      color: "#c7c7c7",
+      highlight: "#616774",
+      label: "Economic Development"
+    },
+    {
+      value: 1,
+      color: "#d7d7d7",
+      highlight: "#616774",
+      label: "Policy and Administration"
+    }
+
+
+  ];
+  var capitalData = [
+    {
+      value: 6,
+      color:"#F7464A",
+      highlight: "#FF5A5E",
+      label: "Public Safety"
+    },
+    {
+      value: 26,
+      color: "#46BFBD",
+      highlight: "#5AD3D1",
+      label: "Neighborhood and Infrastructure"
+    },
+    {
+      value: 3,
+      color: "#FDB45C",
+      highlight: "#FFC870",
+      label: "General Government"
+    },
+    {
+      value: 10,
+      color: "#949FB1",
+      highlight: "#A8B3C5",
+      label: "Recreation and Culture"
+    },
+    {
+      value: 43,
+      color: "#4D5360",
+      highlight: "#616774",
+      label: "Transportation"
+    },
+    {
+      value: 11,
+      color: "#666",
+      highlight: "#616774",
+      label: "Health and Human Services"
+    },
+    {
+      value: 1,
+      color: "#c7c7c7",
+      highlight: "#616774",
+      label: "Economic Development"
+    },
+    {
+      value: 0,
+      color: "#d7d7d7",
+      highlight: "#616774",
+      label: "Policy and Administration"
+    }
+
+
+  ];
+  var totalData = [
+    {
+      value: 23,
+      color:"#F7464A",
+      highlight: "#FF5A5E",
+      label: "Public Safety"
+    },
+    {
+      value: 23,
+      color: "#46BFBD",
+      highlight: "#5AD3D1",
+      label: "Neighborhood and Infrastructure"
+    },
+    {
+      value: 6,
+      color: "#FDB45C",
+      highlight: "#FFC870",
+      label: "General Government"
+    },
+    {
+      value: 6,
+      color: "#949FB1",
+      highlight: "#A8B3C5",
+      label: "Recreation and Culture"
+    },
+    {
+      value: 29,
+      color: "#4D5360",
+      highlight: "#616774",
+      label: "Transportation"
+    },
+    {
+      value: 10,
+      color: "#666",
+      highlight: "#616774",
+      label: "Health and Human Services"
+    },
+    {
+      value: 3,
+      color: "#c7c7c7",
+      highlight: "#616774",
+      label: "Economic Development"
+    },
+    {
+      value: 1,
+      color: "#d7d7d7",
+      highlight: "#616774",
+      label: "Policy and Administration"
+    }
+  ];
