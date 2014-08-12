aa16791ece126fcfdd13460c5e42d439b60eaa27
dannysepler <dsepler@ufl.edu>
2014-08-12 03:15:16 -0400

    finished bar graphs, percentages still need work though

---
+++ bootstrapped/bar-data.js
@@ -0,0 +1,92 @@
+var operating_data = {
+  labels : [
+    "Public Safety (29%)", 
+    "Transportation (24%)",
+    "Neighborhood and Infrastructure (22%)",
+    "Human and Health Services (9%)", 
+    "General Government (7%)", 
+    "Recreation and Culture (5%)", 
+    "Economic Development (3%)",
+    "Policy/Administration (1%)"
+  ], datasets : [
+    {
+      fillColor : "rgba(220,220,220,0.5)",
+      strokeColor : "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+      data : 
+      [
+        1318.0906,   // public safety
+        1090.8336,    // transportation
+        999.9308,    // neighborhood and infrastructure
+        409.0626,    // human and health
+        136.3542,    // general government
+        227.257,    // rec and culture
+        136.3542,     // economic development
+        45.4514    // policy/administration
+      ]
+    }
+  ]
+};
+
+var capital_data = {
+  labels : [
+    "Transportation (43%)",
+    "Neighborhood and Infrastructure (26%)",
+    "Human and Health Services (11%)", 
+    "Recreation and Culture (10%)", 
+    "Public Safety (6%)", 
+    "General Government (3%)", 
+    "Economic Development (1%)",
+    "Policy/Admin (0%)"
+  ], datasets : [
+    {
+      fillColor : "rgba(220,220,220,0.5)",
+      strokeColor : "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+      data : [
+        708.49,   // transportation
+        426.117,    // neighborhood
+        178.076,    // human and health
+        169.459,    // rec and culture
+        93.445,    // public safety
+        52.105,    // general government
+        21.051,     // economic development
+        0         // policy/admin
+      ]
+    }
+  ]
+};
+
+
+var total_data = {
+  labels : [
+    "Transportation (24%)",
+    "Neighborhood and Infrastructure (22%)",
+    "Public Safety (29%)", 
+    "Human and Health Services (9%)", 
+    "Recreation and Culture (5%)", 
+    "General Government (7%)", 
+    "Economic Development (3%)",
+    "Policy/Administration (1%)"
+  ], datasets : [
+    {
+      fillColor : "rgba(220,220,220,0.5)",
+      strokeColor : "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+      data : 
+      [
+        1799.9036,    // transportation
+        1428.6708,    // neighborhood and infrastructure
+        1417.0306,   // public safety
+        590.4526,    // human and health
+        392.157,    // rec and culture
+        367.6298,    // general government
+        152.8442,     // economic development
+        45.4514    // policy/administration
+      ]
+    }
+  ]
+};
