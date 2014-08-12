191e33e96f13c9f951c757cd1b517c9130f45006
dannysepler <dsepler@ufl.edu>
2014-08-12 11:23:46 -0400

    moved html files to root directory

--- data/doughnut-data.js
+++ data/doughnut-data.js
@@ -1,48 +1,56 @@
 var operatingData = [
     {
       value: 29,
+      // dollarValue: 0,
       color:"#F7464A",
       highlight: "#FF5A5E",
       label: "Public Safety"
     },
     {
       value: 22,
+      // dollarValue: 0,
       color: "#46BFBD",
       highlight: "#5AD3D1",
       label: "Neighborhood and Infrastructure"
     },
     {
       value: 7,
+      // dollarValue: 0,
       color: "#FDB45C",
       highlight: "#FFC870",
       label: "General Government"
     },
     {
       value: 5,
+      // dollarValue: 0,
       color: "#949FB1",
       highlight: "#A8B3C5",
       label: "Recreation and Culture"
     },
     {
       value: 24,
+      // dollarValue: 0,
       color: "#4D5360",
       highlight: "#616774",
       label: "Transportation"
     },
     {
       value: 9,
+      // dollarValue: 0,
       color: "#666",
       highlight: "#616774",
       label: "Health and Human Services"
     },
     {
       value: 3,
+      // dollarValue: 0,
       color: "#c7c7c7",
       highlight: "#616774",
       label: "Economic Development"
     },
     {
       value: 1,
+      // dollarValue: 0,
       color: "#d7d7d7",
       highlight: "#616774",
       label: "Policy and Administration"
@@ -53,48 +61,56 @@ var operatingData = [
   var capitalData = [
     {
       value: 6,
+      // dollarValue: 0,
       color:"#F7464A",
       highlight: "#FF5A5E",
       label: "Public Safety"
     },
     {
       value: 26,
+      // dollarValue: 0,
       color: "#46BFBD",
       highlight: "#5AD3D1",
       label: "Neighborhood and Infrastructure"
     },
     {
       value: 3,
+      // dollarValue: 0,
       color: "#FDB45C",
       highlight: "#FFC870",
       label: "General Government"
     },
     {
       value: 10,
+      // dollarValue: 0,
       color: "#949FB1",
       highlight: "#A8B3C5",
       label: "Recreation and Culture"
     },
     {
       value: 43,
+      // dollarValue: 0,
       color: "#4D5360",
       highlight: "#616774",
       label: "Transportation"
     },
     {
       value: 11,
+      // dollarValue: 0,
       color: "#666",
       highlight: "#616774",
       label: "Health and Human Services"
     },
     {
       value: 1,
+      // dollarValue: 0,
       color: "#c7c7c7",
       highlight: "#616774",
       label: "Economic Development"
     },
     {
       value: 0,
+      // dollarValue: 0,
       color: "#d7d7d7",
       highlight: "#616774",
       label: "Policy and Administration"
@@ -105,48 +121,56 @@ var operatingData = [
   var totalData = [
     {
       value: 23,
+      // dollarValue: 0,
       color:"#F7464A",
       highlight: "#FF5A5E",
       label: "Public Safety"
     },
     {
       value: 23,
+      // dollarValue: 0,
       color: "#46BFBD",
       highlight: "#5AD3D1",
       label: "Neighborhood and Infrastructure"
     },
     {
       value: 6,
+      // dollarValue: 0,
       color: "#FDB45C",
       highlight: "#FFC870",
       label: "General Government"
     },
     {
       value: 6,
+      // dollarValue: 0,
       color: "#949FB1",
       highlight: "#A8B3C5",
       label: "Recreation and Culture"
     },
     {
       value: 29,
+      // dollarValue: 0,
       color: "#4D5360",
       highlight: "#616774",
       label: "Transportation"
     },
     {
       value: 10,
+      // dollarValue: 0,
       color: "#666",
       highlight: "#616774",
       label: "Health and Human Services"
     },
     {
       value: 3,
+      // dollarValue: 0,
       color: "#c7c7c7",
       highlight: "#616774",
       label: "Economic Development"
     },
     {
       value: 1,
+      // dollarValue: 0,
       color: "#d7d7d7",
       highlight: "#616774",
       label: "Policy and Administration"
